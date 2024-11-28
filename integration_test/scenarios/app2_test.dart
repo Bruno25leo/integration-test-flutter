1e47a02c89b025d8bd3dc6c7195e707652056dc1
Bruno Ribeiro <bruno25leo@gmail.com>
2024-11-27 23:44:46 -0300

    CI: tests with flutter drive

--- integration_test/scenarios/app2_test.dart
+++ integration_test/scenarios/app2_test.dart
@@ -27,25 +27,5 @@ void main() {
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
     });
-    testWidgets('error test',
-        (tester) async {
-      // Load app widget.
-      await tester.pumpWidget(const MyApp());
-
-      // Verify the counter starts at 0.
-      expect(find.text('Gato'), findsOneWidget);
-
-      // Finds the floating action button to tap on.
-      final fab = find.byKey(const ValueKey('increment'));
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab);
-
-      // Trigger a frame.
-      await tester.pumpAndSettle();
-
-      // Verify the counter increments by 1.
-      expect(find.text('1'), findsOneWidget);
-    });
   });
 }
---
+++ integration_test/scenarios/main.dart
@@ -0,0 +1,9 @@
+import "app_test.dart" as test1;
+import "app2_test.dart" as test2;
+import "app3_test.dart" as test3;
+
+void main() {
+  test1.main();
+  test2.main();
+  test3.main();
+}
